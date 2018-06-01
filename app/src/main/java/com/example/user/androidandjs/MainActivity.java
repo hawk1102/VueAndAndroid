d2b3bee002e94d7d5d9ae482c671ba92b70e3b3f
zhiwei <deva1213082309@gmail.com>
2018-06-01 09:15:36 +0800

    second commit

--- app/src/main/java/com/example/user/androidandjs/MainActivity.java
+++ app/src/main/java/com/example/user/androidandjs/MainActivity.java
@@ -12,8 +12,9 @@ import com.google.gson.Gson;
 public class MainActivity extends AppCompatActivity {
 
     private WebView webView;
-   // private String url="http://10.40.20.64:8888/html/html/borrowRecordone.html";
-   // private String url="http://10.40.20.110:8080/fenqi-majia-web/1.4.4/html/borrowRecordone.html";
+
+    // private String url="http://10.40.20.64:8888/html/html/borrowRecordone.html";
+    // private String url="http://10.40.20.110:8080/fenqi-majia-web/1.4.4/html/borrowRecordone.html";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,28 +22,27 @@ public class MainActivity extends AppCompatActivity {
         webView = findViewById(R.id.web);
         webView.setWebChromeClient(new WebChromeClient());
         webView.getSettings().setJavaScriptEnabled(true);
-        webView.addJavascriptInterface(new MyBridge(this),"bridge" );
-       // webView.loadUrl("file:///android_asset/javascript.html");
-        webView.loadUrl("file:///android_asset/borrowRecordone.html");
-      //  webView.loadUrl(url);
+        webView.addJavascriptInterface(new MyBridge(this), "bridge");
+         webView.loadUrl("file:///android_asset/javascript.html");
+      //  webView.loadUrl("file:///android_asset/borrowRecordone.html");
+        //  webView.loadUrl(url);
 
 
-        Button btn=findViewById(R.id.btn);
+        Button btn = findViewById(R.id.btn);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-               // webView.loadUrl("javascript:callJS()");
-                Person person=new Person();
+                // webView.loadUrl("javascript:callJS()");
+                Person person = new Person();
                 person.setName("zitan");
                 person.setAge(27);
                 person.setBoy(true);
                 String s = new Gson().toJson(person);
-                webView.loadUrl("javascript:jsGetMessageFromNative("+123+")");
+//                webView.loadUrl("javascript:jsGetMessageFromNative(" + 123 + ")");
+                webView.loadUrl("javascript:callJS()");
 
             }
         });
-
-
 
 
     }
